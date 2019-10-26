7bddff4bc3ba2eb852b6bed22a0fe19284fab0ec
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2019-10-26 07:15:13 +0530

    Modified 1008A.

--- 1008A.py
+++ 1008A.py
@@ -2,9 +2,14 @@ s = input().lower()
 con = list('bcdfghjklmnpqrstvwxyz')
 vow = list("aeiou")
 flag = 1
-for i in range(len(s)):
-    if s[i] in con and s[i]!='n':
-        if s[i+1] not in vow:
-            flag = 0
-            break
-print ("YES" if flag else "NO")
+if len(s)==1 and s in con and s!='n':
+    print ("NO")
+elif s[len(s)-1] in con and s[len(s)-1]!='n':
+    print ("NO")
+else:
+    for i in range(len(s)):
+        if s[i] in con and s[i]!='n':
+                if s[i+1] not in vow:
+                    flag = 0
+                    break
+    print ("YES" if flag else "NO")
